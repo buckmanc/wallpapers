91be45efa0f78049d302c25ac18a57a58060423e
Charles Buckman <buckmanc@gmail.com>
2023-11-07 00:59:00 -0600

    add some attribution, new wallpapers

--- mobile/stormlight/attrib.md
+++ mobile/stormlight/attrib.md
@@ -1,2 +1,24 @@
-Herald_of_Justice.jpg Herald of Justice by Petar Penev
-kal_and_syl_by_inawong.jpg [Kal and Syl](https://www.deviantart.com/inawong/art/Kal-and-Syl-699058325) by Ina Wong
+dalinar_by_petar_penev.jpg Dalinar by Petar Penev
+mastermind_by_ari_ibarra.jpg Mastermind by Ari Ibarra
+chanarach_by_karla_ortiz.png Chanarach by Karla Ortiz
+stormblessed_by_air_ibarra.jpg Stormblessed by Air Ibarra
+dalinar_by_randy_vargas.jpg Dalinar by Randy Vargas
+eshonai_by_antti_hakosaari.jpg Eshonai by Antti Hakosaari
+herald_of_justice_by_petar_penev.jpg Herald of Justice by Petar Penev
+ishar_by_dan_dos_santos.png Ishar by Dan dos Santos
+jezrien_by_howard_lyon.jpg Jezrien by Howard Lyon
+kalak_by_magali_villeneuve.jpg Kalak by Magali Villeneuve
+nale_by_karla_ortiz.png Nale by Karla Ortiz
+pailiah_by_magali_villeneuve.jpg Pailiah by Magali Villeneuve
+radiant_by_jemma_young.jpg Radiant by Jemma Young
+rider_of_storms_by_connor_chamberlain.jpg Rider of Storms by Connor Chamberlain
+shalash_by_dan_dos_santos.png Shalash by Dan dos Santos
+taln_by_donato_giancola.jpg Taln by Donato Giancola
+tanavast_by_ari_ibarra.jpg Tanavast by Ari Ibarra
+vev_by_howard_lyon.jpg Vev by Howard Lyon
+visions_of_the_future_by_ari_ibarra.jpg Visions of the Future by Ari Ibarra
+artifabrian_by_ari_ibarra.jpg Artifabrian by Ari Ibarra
+cultivation_and_the_nightwatcher_by_ari_ibarra.jpg Cultivation and the Nightwatcher by Ari Ibarra
+odium_reigns_by_ari_ibarra.jpg Odium Reigns by Ari Ibarra
+kal_and_syl_by_ina_wong.jpg [Kal and Syl](https://www.deviantart.com/inawong/art/Kal-and-Syl-699058325) by Ina Wong
+kaladin_by_antti_hakosaari.jpg Kaladin by Antti Hakosaari
---
+++ rename_to_attrib.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+set -e
+
+if [ -z "$1" ]
+then
+	echo 'need some paths'
+	exit 1
+fi
+
+# stackoverflow.com/a/296135731995812
+quoteRe() {
+	sed -e 's/[^^]/[&]/g; s/\^/\\^/g; $!a\'$'\n''\\n' <<<"$1" | tr -d '\n'
+}
+
+total=$(echo "$1" | wc -l)
+
+echo "$1" | while read -r src; do
+	filename="$(basename "$src")"
+
+	if [ -z "$filename" ]
+	then
+		continue
+	fi
+
+	((i++)) || true
+	src_dir="$(dirname "$src")"
+	src_ext="${src##*.}"
+	printf '%4d/%d: %s... ' "$i" "$total" "$filename"
+
+	dirReadmePath="$(dirname "$src")/README.MD"
+	dirAttribPath="$(dirname "$src")/attrib.md"
+	if [ -f "$dirAttribPath" ]
+	then
+		attrib="$(grep -iPo "(?<=$(quoteRe "$filename")\s).+$" "$dirAttribPath")" || true
+	fi
+
+	# echo "dirAttribPath: $dirAttribPath"
+	# echo "attrib: $attrib"
+
+	if [ -z "$attrib" ]
+	then
+		echo "no attrib"
+		continue
+	fi
+
+	newname="$(echo "${attrib,,}" | sed 's/([^)]*)//g' | sed 's/[][]//g' | inline-detox).${src_ext,,}"
+
+	mainReadmePath="$(git rev-parse --show-toplevel)/README.MD"
+	thumb_dir="$(git rev-parse --show-toplevel)/thumbnails"
+	thumbnail_old="$(find "$thumb_dir" -type f -name "$filename" | head -n 1)"
+	thumbnail_new="$(dirname "$thumbnail_old")/${newname}"
+	src_new="${src_dir}/${newname}"
+	filename_escaped="${filename// /%20}"
+
+	if [ "$filename" = "$newname" ]
+	then
+		echo "skipped!"
+		continue
+	fi
+
+	# echo
+	# echo "thumbnail_old: $thumbnail_old"
+	# echo "thumbnail_new: $thumbnail_new"
+
+	git mv "$thumbnail_old" "$thumbnail_new"
+	git mv "$src" "$src_new"
+	sed -i "s/$(quoteRe "$filename")/$newname/g" "$dirAttribPath"
+	sed -i "s/$(quoteRe "$filename_escaped")/$newname/g" "$dirReadmePath"
+	sed -i "s/$(quoteRe "$filename_escaped")/$newname/g" "$mainReadmePath"
+
+	echo "updated!"
+
+
+done
+
