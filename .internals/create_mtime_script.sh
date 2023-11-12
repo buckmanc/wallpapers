8e72b71891cc0c60f6286eaaac4a69431191f5c0
Charles Buckman <buckmanc@gmail.com>
2023-11-12 07:32:03 -0600

    consolidate update script

--- .internals/create_mtime_script.sh
+++
@@ -1,5 +0,0 @@
-
-
-git ls-files | while read -r src; do
-	echo "touch -c -d $(date -r "$src" "+%F") \"$src\""
-done
--- update.sh
+++ update.sh
@@ -1,5 +1,14 @@
 set -e
 
+create_mtime_script(){
+	git ls-files | while read -r src; do
+		if [ -f "$src" ]
+		then
+			echo "touch -c -d $(date -r "$src" "+%F") \"$src\""
+		fi
+	done
+}
+
 gitroot=$(git rev-parse --show-toplevel)
 
 git fetch --depth 1
@@ -11,7 +20,7 @@ else
 	# otherwise the entire repo shows as new every update
 
 	echo "logging file modification times"
-	mtimeScript=$("$gitroot/.internals/create_mtime_script.sh")
+	mtimeScript=$(create_mtime_script)
 	git reset --hard origin/main
 	git clean -dfx
 	git gc --prune=all
