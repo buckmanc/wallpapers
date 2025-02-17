43f87b14b0d2605953a236925d0b3ba8fc93f521
Charles Buckman <buckmanc@gmail.com>
2025-02-17 05:05:00 -0600

    smooth out updates

--- update.sh
+++ update.sh
@@ -10,6 +10,8 @@ if [[ $(git rev-parse HEAD) == $(git rev-parse @{u}) ]]
 then
 	echo "already up to date"
 else
+	# smooth out unattended updates
+	rm -f "$gitRoot/.git/index.lock"
 	git reset --hard origin/main
 	git clean -dfx
 	git gc --prune=all
